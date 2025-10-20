91bcac12771a3961b60899a46b2513a521c8683b
BrunoSoaresDPS <shbruno66@gmail.com>
2025-10-20 19:51:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-base: '/iveco-warranty-bot/',
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/iveco-warranty-bot/',
   server: {
     host: "::",
     port: 8080,
